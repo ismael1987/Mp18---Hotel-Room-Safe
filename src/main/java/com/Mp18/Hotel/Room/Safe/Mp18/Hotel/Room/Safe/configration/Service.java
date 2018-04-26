af9bc2ce3406ff3f82e1e90466001925e224c6b1
ismael1987 <asmael-1987@hotmail.com>
2018-04-26 15:35:32 +0200

    hotel room

---
+++ src/main/java/com/Mp18/Hotel/Room/Safe/Mp18/Hotel/Room/Safe/configration/Service.java
@@ -0,0 +1,15 @@
+package com.Mp18.Hotel.Room.Safe.Mp18.Hotel.Room.Safe.configration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+public class Service implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("home");
+    }
+}
